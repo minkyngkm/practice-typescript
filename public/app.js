3340586abca7ca1b9fb6ab9ac71add94f7be3e1a
Minkyung Kim <min@Minkyungui-MacBookPro.local>
2020-08-30 11:19:30 +0900

    make a class

--- public/app.js
+++ public/app.js
@@ -1,4 +1,28 @@
 "use strict";
+// class ( to craete objects with the same format )
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c,
+            this.details = d,
+            this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice("Jane", "Cleaning", 200);
+var invTwo = new Invoice("Kim", "babysitting", 100);
+// console.log( invOne,invTwo)
+// make an array that has instances 
+var invoices = [];
+// type 지정하고, declare 까지 한 거.
+invoices.push(invOne);
+invoices.push(invTwo);
+// console.log( invoices)
+invoices.forEach(function (inv) {
+    return console.log(inv.client, inv.details, inv.amount, inv.format());
+});
 var form = document.querySelector(".new-item-form");
 var type = document.querySelector("#type");
 var toForm = document.querySelector("#tofrom");
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,40 @@
+// class ( to craete objects with the same format )
+
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor ( c: string, d: string, a: number) {
+        this.client = c,
+        this.details = d,
+        this.amount = a
+    }
+    format(){
+        return `${this.client} owes £${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice( "Jane", "Cleaning", 200)
+const invTwo = new Invoice( "Kim", "babysitting", 100)
+
+// console.log( invOne,invTwo)
+
+// make an array that has instances 
+
+let invoices : Invoice[] = []
+// type 지정하고, declare 까지 한 거.
+
+invoices.push(invOne)
+invoices.push(invTwo)
+
+// console.log( invoices)
+
+invoices.forEach( inv => 
+    console.log( inv.client, inv.details, inv.amount, inv.format())
+    )
+
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement
 
 const type = document.querySelector("#type") as HTMLSelectElement
